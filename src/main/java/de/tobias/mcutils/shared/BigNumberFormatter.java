81ab93c1610b9a8ae240e2cc573cdbf33f210fb8
Tobias Dickes <hanstodi2@gmail.com>
2022-09-29 13:46:12 +0200

    Add some number formatters

---
+++ src/main/java/de/tobias/mcutils/shared/BigNumberFormatter.java
@@ -0,0 +1,44 @@
+package de.tobias.mcutils.shared;
+
+public class BigNumberFormatter {
+
+    public String THOUSAND_SHORT = "k";
+    public String MILLION_SHORT = "M";
+    public String BILLION_SHORT = "M";
+
+    public int THOUSAND_DECIMALS = 2;
+    public int MILLION_DECIMALS = 3;
+    public int BILLION_DECIMALS = 5;
+
+    public BigNumberFormatter() {}
+
+    public String formatLong(Long number) {
+        if (number == Long.MAX_VALUE) return "MAX";
+        if (number >= 1000 * 1000 * 1000)
+            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000 * 1000), BILLION_DECIMALS) + BILLION_SHORT;
+        if (number >= 1000 * 1000)
+            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000), MILLION_DECIMALS) + MILLION_SHORT;
+        if (number >= 1000) return NumberUtils.stringRoundDouble(number / (1000.0), THOUSAND_DECIMALS) + THOUSAND_SHORT;
+        return number.toString();
+    }
+
+    public String formatDouble(Double number) {
+        if (number == Double.MAX_VALUE) return "MAX";
+        if (number >= 1000 * 1000 * 1000)
+            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000 * 1000), BILLION_DECIMALS) + BILLION_SHORT;
+        if (number >= 1000 * 1000)
+            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000), MILLION_DECIMALS) + MILLION_SHORT;
+        if (number >= 1000) return NumberUtils.stringRoundDouble(number / (1000.0), THOUSAND_DECIMALS) + THOUSAND_SHORT;
+        return number.toString();
+    }
+
+    public String formatInteger(Integer number) {
+        if (number == Integer.MAX_VALUE) return "MAX";
+        if (number >= 1000 * 1000 * 1000)
+            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000 * 1000), BILLION_DECIMALS) + BILLION_SHORT;
+        if (number >= 1000 * 1000)
+            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000), MILLION_DECIMALS) + MILLION_SHORT;
+        if (number >= 1000) return NumberUtils.stringRoundDouble(number / (1000.0), THOUSAND_DECIMALS) + THOUSAND_SHORT;
+        return number.toString();
+    }
+}
---
+++ src/main/java/de/tobias/mcutils/shared/NumberUtils.java
@@ -0,0 +1,25 @@
+package de.tobias.mcutils.shared;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class NumberUtils {
+
+    public static String stringRoundLong(Long value, int places) {
+        DecimalFormat df = new DecimalFormat("#." + ("#".repeat(places)));
+        df.setRoundingMode(RoundingMode.CEILING);
+        return df.format(value);
+    }
+
+    public static String stringRoundDouble(Double value, int places) {
+        DecimalFormat df = new DecimalFormat("#." + ("#".repeat(places)));
+        df.setRoundingMode(RoundingMode.CEILING);
+        return df.format(value);
+    }
+
+    public static String stringRoundFloat(Float value, int places) {
+        DecimalFormat df = new DecimalFormat("#." + ("#".repeat(places)));
+        df.setRoundingMode(RoundingMode.CEILING);
+        return df.format(value);
+    }
+}
