464f47d3053c50e6bd2f4bf47ae8f879f399a6cb
Tobias Dickes <hanstodi2@gmail.com>
2022-10-02 00:47:07 +0200

    Add some fancy new numbers

--- src/main/java/de/tobias/mcutils/shared/BigNumberFormatter.java
+++ src/main/java/de/tobias/mcutils/shared/BigNumberFormatter.java
@@ -9,7 +9,14 @@ public class BigNumberFormatter {
     public String BILLION_SHORT = "B";
     public String TRILLION_SHORT = "T";
     public String QUADRILLION_SHORT = "QD";
+    public String QUINTILLION_SHORT = "QT";
+    public String SEXTILLION_SHORT = "SEXT";
+    public String SEPTILLION_SHORT = "SEPT";
+    public String OCTILLION_SHORT = "OCT";
+    public String NONILLION_SHORT = "OCT";
 
+
+    public int OTHER_DECIMALS = 3;
     public int THOUSAND_DECIMALS = 2;
     public int MILLION_DECIMALS = 3;
     public int BILLION_DECIMALS = 3;
@@ -21,52 +28,46 @@ public class BigNumberFormatter {
 
     public String formatLong(Long number) {
         if (number == Long.MAX_VALUE) return "MAX";
-        if (number >= 1000 * 1000 * 1000)
-            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000 * 1000), BILLION_DECIMALS) + BILLION_SHORT;
-        if (number >= 1000 * 1000)
-            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000), MILLION_DECIMALS) + MILLION_SHORT;
-        if (number >= 1000) return NumberUtils.stringRoundDouble(number / (1000.0), THOUSAND_DECIMALS) + THOUSAND_SHORT;
-        return number.toString();
+        return formatBigDecimal(new BigDecimal(number));
     }
 
     public String formatDouble(Double number) {
         if (number == Double.MAX_VALUE) return "MAX";
-        if (number >= 1000 * 1000 * 1000)
-            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000 * 1000), BILLION_DECIMALS) + BILLION_SHORT;
-        if (number >= 1000 * 1000)
-            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000), MILLION_DECIMALS) + MILLION_SHORT;
-        if (number >= 1000) return NumberUtils.stringRoundDouble(number / (1000.0), THOUSAND_DECIMALS) + THOUSAND_SHORT;
-        return NumberUtils.stringRoundDouble(number/1.0, NORMAL_DECIMALS);
+        return formatBigDecimal(new BigDecimal(number));
     }
 
     public String formatInteger(Integer number) {
         if (number == Integer.MAX_VALUE) return "MAX";
-        if (number >= 1000 * 1000 * 1000)
-            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000 * 1000), BILLION_DECIMALS) + BILLION_SHORT;
-        if (number >= 1000 * 1000)
-            return NumberUtils.stringRoundDouble(number / (1000.0 * 1000), MILLION_DECIMALS) + MILLION_SHORT;
-        if (number >= 1000) return NumberUtils.stringRoundDouble(number / (1000.0), THOUSAND_DECIMALS) + THOUSAND_SHORT;
-        return NumberUtils.stringRoundDouble(number/1.0, NORMAL_DECIMALS);
+        return formatBigDecimal(new BigDecimal(number));
+    }
+
+    /* Thanks to: https://www.turito.com/blog/one-on-one-online-tutoring/what-comes-after-trillion :) */
+    public String getShortByExponentialCount(Integer exponent) {
+        if(exponent >= 45) return "QTT";
+        if(exponent >= 42) return "TRE";
+        if(exponent >= 39) return "DUO";
+        if(exponent >= 36) return "UND";
+        if(exponent >= 33) return "DEC";
+        if(exponent >= 30) return "NO";
+        if(exponent >= 27) return "OC";
+        if(exponent >= 24) return "SE";
+        if(exponent >= 21) return "SX";
+        if(exponent >= 18) return "QI";
+        if(exponent >= 15) return "QD";
+        if(exponent >= 12) return "T";
+        if(exponent >= 9) return "B";
+        if(exponent >= 6) return "M";
+        if(exponent >= 3) return "k";
+        return "";
     }
 
     public String formatBigDecimal(BigDecimal number) {
-        if (number.divide(new BigDecimal(1000*1000*1000)).divide(new BigDecimal(1000)).longValue() >= 1000) {
-            return NumberUtils.stringRoundDouble(number.divide(new BigDecimal(1000*1000*1000).multiply(new BigDecimal(1000*1000))).doubleValue(), QUADRILLION_DECIMALS) + QUADRILLION_SHORT;
+        Integer numbersAfterZero = 0;
+        BigDecimal temp = number;
+        while(temp.compareTo(new BigDecimal(1000)) > 0 && !getShortByExponentialCount(numbersAfterZero+3).equalsIgnoreCase("")) {
+            temp = temp.divide(new BigDecimal(1000));
+            numbersAfterZero+=3;
         }
-
-        if (number.divide(new BigDecimal(1000*1000*1000)).longValue() >= 1000) {
-            return NumberUtils.stringRoundDouble(number.divide(new BigDecimal(1000*1000*1000).multiply(new BigDecimal(1000))).doubleValue(), TRILLION_DECIMALS) + TRILLION_SHORT;
-        }
-
-        if (number.divide(new BigDecimal(1000*1000)).longValue() >= 1000) {
-            return NumberUtils.stringRoundDouble(number.divide(new BigDecimal(1000*1000*1000)).doubleValue(), BILLION_DECIMALS) + BILLION_SHORT;
-        }
-
-        if (number.divide(new BigDecimal(1000)).longValue() >= 1000) {
-            return NumberUtils.stringRoundDouble(number.divide(new BigDecimal(1000*1000)).doubleValue(), MILLION_DECIMALS) + MILLION_SHORT;
-        }
-
-        if (number.longValue() >= 1000) return NumberUtils.stringRoundDouble(number.divide(new BigDecimal(1000)).doubleValue(), THOUSAND_DECIMALS) + THOUSAND_SHORT;
-        return NumberUtils.stringRoundDouble(number.doubleValue(), NORMAL_DECIMALS);
+        return NumberUtils.stringRoundDouble(temp.doubleValue(), OTHER_DECIMALS) + getShortByExponentialCount(numbersAfterZero);
     }
 }
